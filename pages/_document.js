1b5a30b6ab16fa8ee0dcdd1857f66066b565c786
Naveen Sharma <naveen.nsit89@gmail.com>
2022-12-30 03:53:47 +0530

    FIRST COMIT

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"></link>
+      <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;display=swap" rel="stylesheet"></link>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
+
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import { Inter } from '@next/font/google' 
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,110 +14,209 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+ 
+       
+      <header>
+        <div className='container'>
+          <div className='header-wrappr'>
+            <div className='logo'>
+              <a href='#'>
+                <img src='/images/logo.svg' alt='' />
+              </a>
+            </div>
+            <div className='right-navigation'>
+              <ul>
+                <li>
+                  <a href=''>Home</a>
+                </li>
+                <li>
+                  <a href=''>Services</a>
+                </li>
+                <li>
+                  <a href=''>Self Help Resources</a>
+                </li>
+                <li>
+                  <a href=''>Contact Us</a>
+                </li>
+                <li>
+                  <a href=''>
+                     <button type='button' className='btn'>BOOK SESSION</button>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
+      </header>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+
+      <div className='banner-container'>
+        <div className='container'>
+          <div className='banner-wrappr'>
+            <div className='banner-txt'>
+              <h1>Building a trusted brand for self help and mental health</h1>
+              <p>Its all in your mind, you can lead the best life with little assit</p>
+              <button type='button' className='btn'>Know More</button>
+            </div>
+            <div className='banner-image'>
+              <img src='/images/banner-image.svg' alt='' />
+            </div>
           </div>
         </div>
+      </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+      <div className='daily-banner-container'>
+        <div className='container'>
+          <div className='daily-banner-wrappr'>
+            <h2>Daily Better</h2>
+            <p className='subtitle'>AnimusViaCredo - Road to mind through Self Awareness and Belief</p>
+            <div className='daily-list-box'>
+              <div className='daily-list'>
+                <h4>Etymology</h4>
+                <p>Animus - Means mind</p>
+                <p>Credo - means personal belief and principles</p>
+                <p>Via - means "by way of", which is from Latin word road (The via Appia was one of the famous highways of ancient Roman times).</p>
+              </div>
+              <div className='daily-list grey'>
+                <h4>Benjamin Disraeli</h4>
+                <p>Nurture your mind with great thoughts, for you will never go any higher than you think </p> 
+              </div>
+              <div className='daily-list'>
+                <h4>Henry Ford</h4>
+                <p>Whether you think you can, or you think you can't, you are right..</p> 
+              </div>
+              <div className='daily-list grey'>
+                <h4>Lao Tzu</h4>
+                <p>Simplicity, patience, compassion. These three are your greatest treasures</p> 
+              </div>
+              <p>
+              We try to make your life better by showing you a mirror of yourself and the helping you on a path of self awareness and Belief. 
+              </p>
+              <p>
+              This finally takes you to a place where you are happy and more content in life. 
+              </p>
+            </div>
+          </div>
         </div>
-      </main>
+      </div>
+
+      <div className='our-section'>
+        <div className='container'>
+          <div className='our-section-wrappr'>
+            <div className='our-item'>
+              <h4>3 Years</h4>
+              <p>of Experince of Life Coaching</p>
+            </div>
+            <div className='our-item'>
+              <h4>1000+</h4>
+              <p>Happy Clients</p>
+            </div>
+            <div className='our-item'>
+              <h4>500+</h4>
+              <p>Better Life</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='make-life-better'>
+          <div className='container'>
+            <div className='make-life-wrappr'>
+              <h2>We can make your daily life better.</h2>
+              <div className='well-being-section'>
+                <div className='well-being-img'>
+                  <img src='/images/well-being.svg' alt='' />
+                </div>
+                <div className='well-being-list'>
+                  <h4>What is mental Well being</h4>
+                  <ul>
+                    <li>To be in a state where i am at ease in my day to day life</li>
+                    <li>To be in a state where i am at peace with me , my environment , my surrounding and my day to day work</li>
+                    <li>To be physically healthy</li>
+                    <li>To be in a state where i am being productive and don't feel stressed</li>
+                    <li>To be in state where i am not dwelling too much on past and not being too anxious about future</li>
+                  </ul>
+                </div> 
+              </div> 
+            </div>
+          </div>
+      </div>
+
+      <div className='make-life-outcome'>
+        <div className='container'>
+        <div className='well-being-section'> 
+                <div className='well-being-list'>
+                  <h4>What is the expected outcome of this exercise</h4>
+                  <ul>
+                    <li>You will feel happier</li>
+                    <li>You will feel in more control of your life</li>
+                    <li>You will do better on your professional front</li>
+                  </ul>
+                </div> 
+                <div className='well-being-img'>
+                  <img src='/images/health-outcome-banner.svg' alt='' />
+                </div>
+              </div>
+        </div>
+      </div>
+
+      <footer>
+        <div className='container'>
+          <div className='footer-wrappr'>
+            <div className='footer-items'>
+              <div className='footer-logo'>
+                <img src='/images/logo-white.svg' alt='' />
+              </div>
+              <p>
+              Building a trusted brand for self help and mental health
+              </p>
+            </div>
+            <div className='footer-items'>
+               <h4>Contact Details</h4>
+               <div className='contact-address'>
+                <div className='adress-1'>
+                  B-101, Ganesh Peth Ln, Ganesh Nagar, Juwekar Wadi, Dadar, Mumbai, Maharashtra 400028
+                </div>
+                <div className='adress-1'>
+                  Phone: +91 9972449749
+                  Mail: mindvoyageofficial@gmail.com
+                </div>
+                <div className='adress-1'>
+                  Mon - Fri: 9am - 6pm <br></br>
+                  Sat & Sun: 11am-7pm
+                </div>
+               </div>
+            </div>
+
+            <div className='footer-items'>
+               <h4>Links</h4>
+               <div className='links'>
+                <ul>
+                  <li><a href='#'>Home</a></li>
+                  <li><a href='#'>Services</a></li>
+                  <li><a href='#'>About Us</a></li>
+                  <li><a href='#'>Self Help Resources</a></li>
+                  <li><a href='#'>Contact Us</a></li>
+                </ul>
+               </div>
+            </div>
+
+            <div className='footer-items'>
+               <h4>Book Appointment</h4>
+               <div className='appointment'>
+                 <div className='appointment-img'>
+                  <img src='/images/telephone-call 1.svg' />
+                 </div>
+                 <div className='appointmenmt-txt'>
+                  <p>Reach Us</p>
+                  <h5>9947773334</h5>
+                 </div>
+               </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+
     </>
   )
 }
